49155d7f221458ef59fb7987bcd736f13c87dd88
Alexandr Khabirov <akhabirov@rallyup.com>
2026-04-05 23:56:55 +0500

    model

---
+++ MarketDataAggregator.Domain/Models/MarketTick.cs
@@ -0,0 +1,11 @@
+﻿namespace MarketDataAggregator.Domain.Models
+{
+    public class MarketTick
+    {
+        public string Ticker { get; init; }
+        public decimal Price { get; init; }
+        public decimal Volume { get; init; }
+        public DateTime Timestamp { get; init; }
+        public string Source { get; init; } 
+    }
+}
